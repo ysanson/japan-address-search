fc32fc0ecca73e452dc231f7a2a4bb5c3b0ac7e7
Yvan Sanson <sansonyvan@gmail.com>
2026-01-20 20:53:39 +0900

    Updated dependencies
    Signed-off-by: Yvan Sanson <sansonyvan@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils', 'shadcn-nuxt']
-})
+				compatibilityDate: "2025-07-15",
+				devtools: { enabled: true },
+				modules: [
+				 "@nuxt/eslint",
+				 "@nuxt/test-utils",
+				 "shadcn-nuxt",
+				 "@nuxtjs/tailwindcss",
+				],
+});
